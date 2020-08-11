b1f2dee1377fab6d00d3a242c88bb22a7727aa38
doobo <doobo@foxmail.com>
2020-08-11 19:30:26 +0800

    项目结构变更

---
+++ ipfs-common/src/test/java/ipfs/common/HttpTests.java
@@ -0,0 +1,37 @@
+package ipfs.common;
+
+import com.alibaba.fastjson.JSON;
+import com.jayway.jsonpath.Configuration;
+import com.jayway.jsonpath.DocumentContext;
+import com.jayway.jsonpath.JsonPath;
+import com.jayway.jsonpath.Option;
+import org.junit.Test;
+import vip.ipav.okhttp.OkHttpClientTools;
+
+import java.io.IOException;
+import java.util.List;
+import java.util.Objects;
+import java.util.stream.Collectors;
+
+public class HttpTests {
+
+	String token = "eyJhbGciOiJSUzI1NiIsInR5cCI6IkpXVCJ9.eyJleHAiOjE1OTcxNDAzNzIsIlRva2VuVHlwZSI6MSwiVG9rZW4iOnsidXNlcl9pZCI6MSwidG9rZW5fdHlwZSI6MSwiY3JlYXRlX3RpbWUiOiIyMDIwLTA4LTExVDE2OjA2OjEyLjI4ODE3NzA1OCswODowMCIsImludmFsaWRfdGltZSI6IjIwMjAtMDgtMTFUMTg6MDY6MTIuMjg4MTc3MjA4KzA4OjAwIiwiYWNjb3VudCI6IiJ9LCJhZGRpdGlvbiI6eyJSZW1vdGVBZGRyZXNzIjoiMTAuMjAuNDkuMjUwIiwiYWNjZXNzX3NpZ24iOiI4Mjg0MzQyMy0zYTRkLTQyOWEtODRmYi03MTlmZmViMjk2ZWYiLCJldWlkIjoiMTAwMDAxMDAwMTAwIiwiZml4VXNlckluZm8iOmZhbHNlLCJsb2dpbl90eXBlIjoxfSwidXNlcl9pbmZvIjp7InVzZXJfaWQiOjEsInVzZXJfbGV2ZWxfaWQiOjEsImFjY291bnQiOiJ0ZXN0IiwiaWRjYXJkIjoiMzMwNzgxMTk4NTA5MDcxMjkwIiwidXNlcl9uYW1lIjoidGVzdCIsImdlbmRlciI6MSwicGhvbmUiOiIiLCJ3b3JrZXJfbm8iOiJ0ZXN0IiwidGhpcmRfYWNjb3VudCI6IiIsImlzX3JlcXVpcmVkX25vdGlmaWNhdGlvbiI6dHJ1ZSwiZGVmYXVsdF9sYW5ndWFnZSI6InpoLWhhbnQiLCJub3RpY2VfdHlwZSI6InNtcyJ9LCJzeXN0ZW1fYWNjb3VudCI6InRlc3QiLCJyZWRpcmVjdF91cmkiOiIiLCJzeXN0ZW1faWQiOjEsImFjY2Vzc19zeXN0ZW1zIjpbeyJhY2NvdW50IjoidGVzdCIsImlzX3N5c3RlbV9hZG1pbiI6IjAiLCJyZWRpcmVjdF91cmkiOiJodHRwOi8vZGV2LmVnb3YtY2hpbmEuY29tL3BzZHMvbG9naW4iLCJzb3J0X2NvZGUiOiItMSIsInN5c3RlbV9jb2RlIjoiYWNjZXB0Iiwic3lzdGVtX2ljb24iOiI5YmZjY2M1ZS0xMzk2LTQ2OTctYTMzMC1lMjhjYjBkN2I5YTMucG5nIiwic3lzdGVtX2lkIjoiMSIsInN5c3RlbV9uYW1lIjoi5Y-X55CG57O757WxIn1dLCJjbGllbnRfaXAiOiIxMC4yMC40OS4yNTAiLCJzaWduIjoiODI4NDM0MjMtM2E0ZC00MjlhLTg0ZmItNzE5ZmZlYjI5NmVmIn0.EgXW-kgToeFmnhv6yeEUfynR2AgD4-3EVg293VpivgQqmmCwjaLCG0WasLB-p1DaBB17T_1WhGQtqqHj1M9rbjfcxPsXw_ICnOHcZ2Df0Cg6uMtGS1t-JbREO9_yYuSU66Cr-swenBXwzHCcStW-Ha7DHis652hXJMVGFwYZCck";
+	Configuration conf = Configuration.builder().build().addOptions(Option.DEFAULT_PATH_LEAF_TO_NULL);
+
+	@Test
+	public void testHttpJson() throws IOException {
+		String res = OkHttpClientTools.getInstance()
+			.post()
+			.url("http://127.0.0.1:9048/api/Affairs/GetCheckAffairInfos")
+			.jsonParams("{\"sort_List\":{\"modify_date\":true},\"index\":1,\"size\":600,\"node_standard_code\":0}")
+			.addHeader("Authorization",token)
+			.execute().body().string();
+		System.out.println(res);
+		DocumentContext doc = JsonPath.using(conf).parse(res);
+		List<String> ls = doc.read("$.data..id");
+		if(ls != null && !ls.isEmpty()){
+			ls = ls.stream().distinct().filter(Objects::nonNull).sorted().collect(Collectors.toList());
+			ls.forEach(m-> System.out.println(m));
+		}
+	}
+}
--- ipfs-gateway/src/main/java/com/github/doobo/config/InitIpfsProject.java
+++ ipfs-gateway/src/main/java/com/github/doobo/config/InitIpfsProject.java
@@ -1,103 +1,21 @@
 package com.github.doobo.config;
 
-import com.github.doobo.utils.FileUtils;
-import com.github.doobo.utils.OsUtils;
-import com.github.doobo.utils.TerminalUtils;
-import lombok.extern.slf4j.Slf4j;
+import com.github.doobo.soft.InitUtils;
 import org.springframework.stereotype.Component;
 
 import javax.annotation.PostConstruct;
-import java.io.File;
-import java.io.FileOutputStream;
-import java.io.OutputStream;
 
 /**
  * ipfs环境初始化
  */
-@Slf4j
 @Component
 public class InitIpfsProject {
-
-	/**
-	 * ipfs命令
-	 */
-	public static String IPFS;
 
 	/**
 	 * 初始化系统环境
 	 */
 	@PostConstruct
 	public void initIpfsEnv(){
-		FileUtils.createFile(".ipfs", ".initIpfs");
-		switch (OsUtils.getSystemType()){
-			case MAC_OS:
-				initMac64Ipfs();
-				break;
-			case Linux:
-				initLinux64Ipfs();
-				break;
-			case Windows:
-				initWin64Ipfs();
-			default:
-				log.warn("not support system env, can't init ipfs");
-		}
-	}
-
-	/**
-	 * 初始化windows环境
-	 * @return
-	 */
-	public boolean initWin64Ipfs(){
-		FileUtils.createFile(".ipfs/go-ipfs", ".goIpfsExe");
-		byte[] rs =FileUtils.readResourcesByte("lib/win64/go-ipfs.zip");
-		try (OutputStream out = new FileOutputStream(".ipfs/go-ipfs/ipfs.exe")){
-			byte[] ipfs = FileUtils.queryFileInZip(rs, "go-ipfs/ipfs.exe");
-			out.write(ipfs);
-			IPFS = new File(".ipfs/go-ipfs/ipfs.exe").getCanonicalPath();
-		} catch (Exception e){
-			log.error("init windows ipfs env fail", e);
-			return false;
-		}
-		return true;
-	}
-
-	/**
-	 * 初始化macOs环境
-	 * @return
-	 */
-	public boolean initMac64Ipfs(){
-		byte[] rs =FileUtils.readResourcesByte("lib/mac64/go-ipfs.tar.gz");
-		return initUnixIpfs(rs);
-	}
-
-	/**
-	 * 初始化Linux环境
-	 */
-	public boolean initLinux64Ipfs(){
-		byte[] rs =FileUtils.readResourcesByte("lib/linux64/go-ipfs.tar.gz");
-		return initUnixIpfs(rs);
-	}
-
-	/**
-	 * linux和mac os下面的ipfs环境初始化
-	 * @param rs
-	 * @return
-	 */
-	private boolean initUnixIpfs(byte[] rs) {
-		try (OutputStream out = new FileOutputStream(".ipfs/go-ipfs.tar.gz")){
-			out.write(rs);
-			out.flush();
-			out.close();
-			String tar = TerminalUtils.execCmd("which tar");
-			if(tar != null && !tar.isEmpty()){
-				TerminalUtils.execCmd("tar zxvf go-ipfs.tar.gz", new File(".ipfs"));
-			}
-			IPFS = new File(".ipfs/go-ipfs/ipfs").getCanonicalPath();
-		} catch (Exception e){
-			log.error("init windows ipfs env fail", e);
-			return false;
-		}
-
-		return true;
+		InitUtils.initIpfsEnv();
 	}
 }
