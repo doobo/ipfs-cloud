e1889338626192e19b021cc87bd7910f572b96e5
doobo <doobo@foxmail.com>
2020-08-19 20:42:09 +0800

    异常配置修复

--- ipfs-client/src/main/java/com/github/doobo/service/IpfsConfigApiFallbackService.java
+++ ipfs-client/src/main/java/com/github/doobo/service/IpfsConfigApiFallbackService.java
@@ -2,10 +2,12 @@ package com.github.doobo.service;
 
 import com.github.doobo.api.IpfsControllerApi;
 import com.github.doobo.conf.IpfsConfig;
+import com.google.common.collect.Lists;
 import feign.hystrix.FallbackFactory;
 import org.springframework.stereotype.Component;
 
 import javax.annotation.Resource;
+import java.util.List;
 
 /**
  * 熔断错误处理
@@ -22,6 +24,11 @@ public class IpfsConfigApiFallbackService implements FallbackFactory<IpfsControl
 			@Override
 			public IpfsConfig getIpfsConfig() {
 				return ipfsConfig;
+			}
+
+			@Override
+			public List<IpfsConfig> queryNodeConfigList() {
+				return Lists.newArrayList(ipfsConfig);
 			}
 		};
 	}
